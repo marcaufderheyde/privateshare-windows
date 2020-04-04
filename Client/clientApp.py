b81e03fb5a55f02c45822c9498e91fa0df47384b
Marc Auf der Heyde <2326744a@student.gla.ac.uk>
2020-04-04 14:05:08 +0100

    Built first GUI for Server and Client. Executables found in dis. Server freezes, currently only issue

---
+++ Client/clientApp.py
@@ -0,0 +1,117 @@
+import PySimpleGUI as sg
+import socket
+import sys
+import os
+
+choices = ('put','get','list')
+
+sg.theme('DarkAmber')   # Add a touch of color
+# All the stuff inside your window.
+layout = [  [sg.Text('Welcome to the privateshare client!')],
+            [sg.Text('Please enter the IP address of the Server:'), sg.InputText()],
+            [sg.Text('Please enter the Port Number of the Server:'), sg.InputText()],
+            [sg.Listbox(choices, size=(15, len(choices)), key='-CHOICE-'), sg.Output(size=(50,10), key='-OUTPUT-')],
+            [sg.Text('Filename you are tyring to download/upload:'), sg.InputText()],
+            [sg.Button('Put/Get/List'), sg.Button('Close Client')] ]
+
+# Create the Window
+window = sg.Window('privateshare client', layout)
+
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    # Create a client socket
+    cli_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    event, values = window.read()
+    server_ip = values[0]
+    server_port = int(values[1])
+    choice = str(values['-CHOICE-'][0])
+    filename = str(values[2])
+    if event in (None, 'Close Client'):   # if user closes window or clicks cancel
+        break
+
+    # Prevent errors
+    try:
+        # Connect client socket to server socket
+        cli_sock.connect((server_ip, server_port))
+        srv_addr = (values[0], int(values[1]))
+
+    except Exception as e:
+        # Print the exception message
+        print(e)
+        
+    try:
+        while True:
+            if choice:
+                if(choice == 'put'):
+                    # Make sure that the client directory contains the file it is trying to upload
+                    directory = os.listdir()
+                    if(not filename in directory):
+                        print("You are trying to upload a file which does not exist. Please try again")
+                        break
+                    else:
+                        # Send the server the exact commands of the put request
+                        cli_sock.sendall((choice + ',' + filename).encode('utf-8'))
+
+                        # Open the file you are trying to send
+                        f = open(filename,'rb')
+                        data = f.read(33554432)
+
+                        # Before starting to send file, check that the server allows it. You cannot upload a file already on the server.
+                        checkreupload = cli_sock.recv(32)
+
+                        if(checkreupload.decode('utf-8') == 'cancel'):
+                            print("You are trying to upload a file with a taken filename. Please alter the filename.")
+                            break
+
+                        # If server allows it, continue to upload file to server.
+                        if(checkreupload.decode('utf-8') == 'pass'):
+                            while(data):
+                                print("Uploading file to server...")
+                                cli_sock.send(data)
+                                data = f.read(33554432)
+                            f.close()
+                            print("Done uploading file to server!")
+                            cli_sock.shutdown(socket.SHUT_WR)
+                            break
+                elif(choice == 'get'):
+                    # Make sure you don't have a file with the same filename already
+                    directory = os.listdir()
+                    if(not filename in directory):
+
+                    # Send the server the exact commands of the get request
+                        cli_sock.send((choice + ',' + filename).encode('utf-8'))
+                        checkreupload = cli_sock.recv(32)
+
+                        # Check that the server has the file you are trying to download
+                        if(checkreupload.decode('utf-8') == 'cancel'):
+                            print("You are trying to download a file that doesn't exist on the server. Please try again!")
+                            break
+
+                        # If the server has the file, continue to download file.
+                        if(checkreupload.decode('utf-8') == 'pass'):
+                            f = open(str(filename),'wb')
+                            data = cli_sock.recv(33554432)
+                            while(data):
+                                print("Receiving...")
+                                f.write(data)
+                                data = cli_sock.recv(33554432)
+                            f.close()
+                            print("Done Receiving")
+                            break
+
+                    else:
+                        print("You already have a file with that filename in the directory you are trying to download into")
+                        break
+
+                elif(choice == 'list'):
+                    print("Reqeusting File Directory from server...")
+                    cli_sock.sendall(choice.encode('utf-8'))
+                    directory = cli_sock.recv(33554432)
+                    print("File Directory received:")
+                    print(directory.decode('utf-8'))
+                    break
+            else:
+                print("You need to select an operation")
+    finally:
+        cli_sock.close()
+window.close()
---
+++ Server/serverApp.py
@@ -0,0 +1,164 @@
+import PySimpleGUI as sg
+import socket
+import sys
+import os
+import time
+
+sg.theme('DarkAmber')   # Add a touch of color
+# All the stuff inside your window.
+layout = [  [sg.Text('Welcome to the privateshare server!')],
+            [sg.Text('Server runs for 5 minutes at a time unless in current operation')],
+            [sg.Text('Please enter the Port Number you wish to bind to:'), sg.InputText()],
+            [sg.Button('Run Server'), sg.Button('Close Server')] ]
+
+# Create the Window
+window = sg.Window('privateshare server', layout)
+
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    # Create server socket
+    srv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    event, values = window.read()
+    port_number = int(values[0])
+
+    """ 
+    Enclose the following two lines in a try-except block to catch
+    exceptions related to already bound ports, invalid/missing
+    command-line arguments, port number out of range, etc.
+    """
+    try:
+        """
+        Register the socket with the OS kernel so that commands sent
+        to the user-defined port number are delivered to this program.
+        """
+        srv_sock.bind(("0.0.0.0", port_number))
+
+        # Report success post-binding
+        print("Server up and running: 0.0.0.0 " + str(port_number) + " Port")
+        print("Waiting for connection")
+        bound = time.time()
+
+        # Initialize connection queue
+        srv_sock.settimeout(300.0)
+        srv_sock.listen(5)
+    except Exception as e:
+        # Print the exception message
+        print(e)
+        # Exit with a non-zero value, to indicate an error condition
+        srv_sock.close()
+
+    if event in (None, 'Close Server'):   # if user closes window or clicks cancel
+        srv_sock.close()
+        break
+
+    while True:
+
+        """
+        Surround the following code in a try-except block to account for
+        socket errors as well as errors related to user input.
+        """
+        try:
+
+            """
+            Dequeue a connection request from the queue created by listen() earlier.
+            If no such request is in the queue yet, this will block until one comes
+            in. Returns a new socket to use to communicate with the connected client
+            plus the client-side socket's address (IP and port number).
+            """
+            cli_sock,cli_adr = srv_sock.accept()
+            print("Connected with" + str(cli_adr))
+
+            # Receive the initial request from client
+            request = cli_sock.recv(32)
+            commands = request.decode('utf-8').split(',')
+
+            # Handling of put request, sever side
+            if (commands[0] == "put"):
+                print("Put request received")
+                directory = os.listdir()
+
+                # Make sure the file is not a reupload, if yes then notify the client to continue
+                if(not commands[1] in directory):
+                    cli_sock.send("pass".encode('utf-8'))
+
+                    # Try opening the file with exclusive creation binary mode
+                    try:
+                        f = open(commands[1],'xb')
+                    except FileExistsError:
+                        print("This file already exists")
+                    data = cli_sock.recv(33554432)
+
+                    # Begin receiving the upload
+                    while(data):
+                        print("Receiving...")
+                        f.write(data)
+                        data = cli_sock.recv(33554432)
+                    f.close()
+                    print("Done Receiving")
+                    cli_sock.close()
+                
+                # If file is a reupload, notify the client and reject the file
+                else:
+                    cli_sock.send("cancel".encode('utf-8'))
+                    time.sleep(1)
+                    print("The server rejected the file. Filename already taken")
+                    cli_sock.close()
+
+            # Handling of get request, server side
+            elif(commands[0] == "get"):
+                print("Get request received")
+                directory = os.listdir()
+
+                # Make sure the file is in the servers directory, if yes then continue accordingly
+                if(commands[1] in directory):
+                    cli_sock.send("pass".encode('utf-8'))
+                    f = open(commands[1],'rb')
+                    print("Sending file to client")
+                    data = f.read(33554432)
+                    while(data):
+                        print("Sending...")
+                        cli_sock.send(data)
+                        data = f.read(33554432)
+                    f.close()
+                    print("Finished sending file to client")
+                    cli_sock.shutdown(socket.SHUT_WR)
+                
+                # Notify client that the file does not exist on the server and close connection
+                else:
+                    cli_sock.send("cancel".encode('utf-8'))
+                    time.sleep(1)
+                    print("The server does not have the file client is attempting to download")
+                    cli_sock.close()
+
+            # Handing of list request, server side
+            elif(commands[0] == "list"):
+                print("List request received")
+                directory = os.listdir()
+                directory_str = ""
+                
+                print("Sending list of File Directory...")
+
+                for file in directory:	
+                    add = file + ", "		
+                    directory_str += add
+
+                cli_sock.send(directory_str.encode('utf-8'))
+                print("Finished sending directory")
+                cli_sock.close()
+
+            else:
+                print("The connection encountered an error. Client must try again...")
+        
+        finally:
+            """
+            If an error occurs or the client closes the connection, call close() on the
+            connected socket to release the resources allocated to it by the OS.
+            """
+            print("The connection has been closed")
+            if 'cli_sock' in globals():
+                cli_sock.close()
+
+    # Close the server socket as well to release its resources back to the OS
+    srv_sock.close()
+
+window.close()
